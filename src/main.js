9d02bd0c835d4da2510e5e7096746af8c206096c
lzq <670123211@qq.com>
2019-08-11 13:17:53 +0800

    init my project

---
+++ src/main.js
@@ -0,0 +1,19 @@
+//入口文件
+import Vue from 'vue'
+//导入MUI样式
+import './lib/mui/css/mui.min.css'
+ 
+//按需导入Mint-ui组件
+import {Header} from 'mint-ui'
+Vue.component(Header.name,Header)
+
+import {Tabbar} from 'mint-ui'
+Vue.component(Tabbar.name,Tabbar)
+
+// 导入App根组件
+import app from './App.vue'
+
+var vm = new Vue({
+  el:'#app',
+  render: c=>c(app)
+})
