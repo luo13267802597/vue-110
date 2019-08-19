1d71469fe088a9755c2180f73b747cb23821198c
lzq <670123211@qq.com>
2019-08-19 13:54:04 +0800

    项目第一天打卡

--- src/main.js
+++ src/main.js
@@ -1,19 +1,40 @@
 //入口文件
 import Vue from 'vue'
+
+//1.1导入路由的包
+ import VueRouter from 'vue-router'
+//  1.2安装路由
+Vue.use(VueRouter)
+
+//2.1导入vue-resource 
+import VueResource from 'vue-resource'
+//2.2安装vue-resource
+Vue.use(VueResource)
+
 //导入MUI样式
 import './lib/mui/css/mui.min.css'
+//导入扩展图标样式
+import './lib/mui/css/icons-extra.css'
  
 //按需导入Mint-ui组件   
-import {Header} from 'mint-ui'
+import {Header,Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 import {Tabbar} from 'mint-ui'
 Vue.component(Tabbar.name,Tabbar)
+//轮播图
+
+
+//1.3导入自己的router.js模块
+import router from './router.js'
 
 // 导入App根组件
 import app from './App.vue'
 
 var vm = new Vue({
   el:'#app',
-  render: c=>c(app)
+  render: c=>c(app),
+  router // 1.4挂载路由对象到路由上
 })
